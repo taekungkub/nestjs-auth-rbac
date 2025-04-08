6b783efd0c1fdb261e01b885800b7e09da5bf9e9
Kieattisak Suparit <kieattisak.sup@devdeva.tech>
2025-04-09 00:25:22 +0700

    chore: add global interceptor

---
+++ src/common/interceptor/response.interceptor.ts
@@ -0,0 +1,53 @@
+// interceptors/response-transform.interceptor.ts
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+// แปลง createdAt และ updatedAt เป็น local time แบบ recursive
+function convertDatesToLocal(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(convertDatesToLocal);
+  } else if (obj && typeof obj === 'object') {
+    const converted = {};
+    for (const key in obj) {
+      if (['createdAt', 'updatedAt'].includes(key) && obj[key]) {
+        converted[key] = dayjs(obj[key])
+          .tz('Asia/Bangkok')
+          .format('YYYY-MM-DD HH:mm:ss');
+      } else {
+        converted[key] = convertDatesToLocal(obj[key]);
+      }
+    }
+    return converted;
+  }
+  return obj;
+}
+
+@Injectable()
+export class ResponseTransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (!data) return data;
+
+        const convertedData = convertDatesToLocal(data.data ?? data);
+
+        return {
+          statusCode: 200,
+          data: convertedData,
+        };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseTransformInterceptor } from './common/interceptor/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   console.log(`🚀 NODE_ENV: ${process.env.NODE_ENV}`);
+  app.useGlobalInterceptors(new ResponseTransformInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
